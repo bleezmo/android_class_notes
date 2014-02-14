8f0dfc4a5c81277cc074c84d67fe53016c7c56d7
bleezmo <joshuarogers10@gmail.com>
2014-02-13 20:54:30 -0500

    notes

---
+++ class_notes/src/com/example/class_notes/NewActivity.java
@@ -0,0 +1,14 @@
+package com.example.class_notes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_activity);
+	}
+
+}
