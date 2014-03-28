97b2bccd4e3a50da75efa959555c7f208555f382
bleezmo <joshuarogers10@gmail.com>
2014-03-27 21:27:37 -0400

    camera example

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-* added custom cursor adapter to display list based on db
-* added click listener so that when clicked, removed item from list
+* sample camera usage
--- class_notes/src/com/example/class_notes/MainActivity.java
+++ class_notes/src/com/example/class_notes/MainActivity.java
@@ -1,67 +1,85 @@
 package com.example.class_notes;
 
-import java.io.IOException;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import utils.Downloader;
-import utils.MyCursorAdapter;
-import utils.SportsManager;
-
-import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
-import android.util.Log;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * list view displaying parsed json stored in an sqlite database
+ * start a camera and display the image taken on the main activity
  * @author josh
  *
  */
-public class MainActivity extends Activity implements Downloader.JSONDownloaderListener{
+public class MainActivity extends Activity {
+	public static final int CAMERA_REQUEST_CODE = 1;
+	//contains path to image
+	private Uri uri;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Downloader.JSONAsyncDownloader(this).execute("http://data.nba.com/json/cms/noseason/scoreboard/20101229/games.json");
     }
-
-	@Override
-	public void onJSONRetrieved(JSONObject json) {
-		try {
-			//get new database object
-			final SportsManager dbmanager = new SportsManager(this);
-			//insert the sports json into the database
-			dbmanager.insertSports(json);
-			//display the database in a list using cursor adapter
-			final ListView lv = (ListView) findViewById(R.id.arenalist);
-			lv.setAdapter(MyCursorAdapter.defaultImplementation(this,dbmanager));
-			//when clicking an item, remove it from the list
-			lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-					SQLiteDatabase db = dbmanager.getWritableDatabase();
-					//delete the record from the database based on the id
-					db.delete(SportsManager.TABLE_NAME, "_id=?", new String[]{Long.toString(id)});
-					//renew the adapter for the listview
-					lv.setAdapter(MyCursorAdapter.defaultImplementation(MainActivity.this, dbmanager));
-				}
-			});
-		} catch (JSONException e1) {
-			e1.printStackTrace();
+    
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == CAMERA_REQUEST_CODE){
+			if(resultCode == RESULT_OK){
+				ImageView iv = (ImageView) findViewById(R.id.camera_img);
+				//display the picture taken in the image view
+				iv.setImageBitmap(BitmapFactory.decodeFile(uri.getPath()));
+			}
 		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
+	public void takePicture(View v){
+		//start the camera via intent
+    	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	uri = getOutputMediaFileUri();
+    	//pass in the path to where the picture should be saved
+    	intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+    	//we pass in request code to retrieve on return
+    	startActivityForResult(intent,CAMERA_REQUEST_CODE);
+    }
+	/**
+	 * generate the file path in which we want to save the image
+	 * @return
+	 */
+    private Uri getOutputMediaFileUri(){
+    	String storageState = Environment.getExternalStorageState();
+    	if(storageState.equals(Environment.MEDIA_MOUNTED)){
+    		File mediaStorageDir = new File(
+    			Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+    			"MyCameraApp");
+    		if(!mediaStorageDir.exists()){
+    			if(!mediaStorageDir.mkdirs()){
+    				Toast.makeText(this, "Failed to create picture directory", Toast.LENGTH_SHORT);
+    				return null;
+    			}
+    		}
+    		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    		File mediaFile = new File(mediaStorageDir.getPath()+File.separator+"IMG_"+timeStamp+".jpg");
+    		return Uri.fromFile(mediaFile);
+    	}else{
+    		Toast.makeText(this, "No SDCard found!", Toast.LENGTH_SHORT).show();
+    		return null;
+    	}
+    }
 }
+
+
+
+
+
+
