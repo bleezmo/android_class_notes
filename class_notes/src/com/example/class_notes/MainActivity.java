d90ec08b1deb38947b8fcbae7bd9e4de1c33fb82
bleezmo <joshuarogers10@gmail.com>
2014-05-06 20:28:20 -0400

    action bar with up navigation

--- class_notes/src/com/example/class_notes/MainActivity.java
+++ class_notes/src/com/example/class_notes/MainActivity.java
@@ -18,7 +18,10 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,17 +29,35 @@ import android.widget.Toast;
 import android.os.Handler;
 
 /**
- * Start a service to run independently of application
+ * start an action bar activity
  * @author josh
  *
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.action_settings){
+			Intent i = new Intent(this,SecondActivity.class);
+			startActivity(i);
+		}
+		return super.onOptionsItemSelected(item);
+	}
+    
+    
 }
 
 
